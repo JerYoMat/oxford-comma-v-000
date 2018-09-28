2a74f9a10868e1360eb5c57f2a9a5a80ad9977b7
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:09:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   last_item = array.last 
   new_array = array.pop 
   new_string = new_array.join(" ")
-  string = new_string + " ," + last_item 
+new_string + " ," + last_item 
   
 end
