648d247f79f6f5127ffd170e5cad8548092b99af
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:19:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
 last_item = array.last.to_s  
 if array.size > 2 
 array.pop 
-array.join(", ") + " and " +  last_item
+array.join(", ") + ", and " +  last_item
 else 
   array.join(" and ")
 end 
