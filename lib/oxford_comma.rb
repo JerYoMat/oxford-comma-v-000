12057443c60db4d0bfc60d8ea81ce33e4af5c289
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:10:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,6 @@ require 'pry'
 def oxford_comma(array)
   last_item = array.last 
   new_array = array.pop 
-  new_string = new_array.join(" ")
-  binding.pry 
-new_string + " ," + last_item 
+  new_array.join(" ")
   
 end
